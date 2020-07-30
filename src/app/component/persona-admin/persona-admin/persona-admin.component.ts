f77bccba03fa1a085d650320d1a9b218cce9800d
ivancas84 <ivancas84@gmail.com>
2020-07-30 10:18:14 -0300

    Desarrollo de modulo de persona-admin

---
+++ src/app/component/persona-admin/persona-admin/persona-admin.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Location } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AdminComponent } from '@component/admin/admin.component';
+import { DataDefinitionService } from '@service/data-definition/data-definition.service';
+import { ValidatorsService } from '@service/validators/validators.service';
+import { SessionStorageService } from '@service/storage/session-storage.service';
+
+@Component({
+  selector: 'app-persona-admin',
+  templateUrl: './persona-admin.component.html',
+})
+export class PersonaAdminComponent extends AdminComponent {
+
+  readonly entityName: string = "persona";
+
+  constructor(
+    protected fb: FormBuilder, 
+    protected route: ActivatedRoute, 
+    protected router: Router, 
+    protected location: Location, 
+    protected dd: DataDefinitionService, 
+    protected validators: ValidatorsService,
+    protected storage: SessionStorageService, 
+    protected dialog: MatDialog,
+    protected snackBar: MatSnackBar
+  ) {
+    super(fb, route, router, location, dd, storage, dialog, snackBar);
+  }
+}
+
---
+++ src/app/component/persona-admin/persona-fieldset/persona-fieldset.component.ts
@@ -0,0 +1,83 @@
+import { Component } from '@angular/core';
+import { FieldsetComponent } from '@component/fieldset/fieldset.component';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DataDefinitionService } from '@service/data-definition/data-definition.service';
+import { ValidatorsService } from '@service/validators/validators.service';
+import { Router } from '@angular/router';
+import { SessionStorageService } from '@service/storage/session-storage.service';
+
+@Component({
+  selector: 'app-persona-fieldset',
+  templateUrl: './persona-fieldset.component.html',
+})
+export class PersonaFieldsetComponent extends FieldsetComponent {
+
+  readonly entityName: string = 'persona';
+
+  constructor(
+    protected fb: FormBuilder, 
+    protected dd: DataDefinitionService, 
+    protected validators: ValidatorsService,
+    protected router: Router, 
+    protected storage: SessionStorageService 
+  ) {
+    super(router, storage);
+  }
+
+  formGroup(): FormGroup {
+    let fg: FormGroup = this.fb.group({
+      id:null,
+      nombres: [null, {
+      }],
+      apellidos: [null, {
+      }],
+      legajo: [null, {
+      }],
+      numero_documento: [null, {
+        validators: [Validators.required],
+      }],
+      telefono_laboral: [null, {
+      }],
+      telefono_particular: [null, {
+      }],
+      fecha_nacimiento: [null, {
+      }],
+      email: [null, {
+      }],
+      cargo: [null, {
+        validators: [Validators.required],
+      }],
+      organo: [null, {
+        validators: [Validators.required],
+      }],
+      departamento_judicial: [null, {
+        validators: [Validators.required],
+      }],
+      departamento_judicial_informado: [null, {
+        validators: [Validators.required],
+      }],
+      tipo_documento: [null, {
+        validators: [Validators.required],
+      }],
+    });
+    return fg;
+  }
+
+  get id() { return this.fieldset.get('id')}
+  get nombres() { return this.fieldset.get('nombres')}
+  get apellidos() { return this.fieldset.get('apellidos')}
+  get legajo() { return this.fieldset.get('legajo')}
+  get numeroDocumento() { return this.fieldset.get('numero_documento')}
+  get telefonoLaboral() { return this.fieldset.get('telefono_laboral')}
+  get telefonoParticular() { return this.fieldset.get('telefono_particular')}
+  get fechaNacimiento() { return this.fieldset.get('fecha_nacimiento')}
+  get email() { return this.fieldset.get('email')}
+  get creado() { return this.fieldset.get('creado')}
+  get eliminado() { return this.fieldset.get('eliminado')}
+  get cargo() { return this.fieldset.get('cargo')}
+  get organo() { return this.fieldset.get('organo')}
+  get departamentoJudicial() { return this.fieldset.get('departamento_judicial')}
+  get departamentoJudicialInformado() { return this.fieldset.get('departamento_judicial_informado')}
+  get tipoDocumento() { return this.fieldset.get('tipo_documento')}
+
+}
