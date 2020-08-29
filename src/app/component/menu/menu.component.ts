09b3fe0198d7bd338fb11badefc86d1e5579e869
ivancas84 <ivancas84@gmail.com>
2020-08-29 15:25:05 -0300

    Agregar valor por defecto al ordenamiento

--- src/app/component/menu/menu.component.ts
+++ src/app/component/menu/menu.component.ts
@@ -1,7 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
 })
-export class MenuComponent { }
+export class MenuComponent implements OnInit { 
+  afiliacionShowQueryParams
+ 
+  ngOnInit() {
+    this.afiliacionShowQueryParams = {
+      modificado_is_set:'false', 
+      order:JSON.stringify({creado:'desc',per_nombres:'asc'})
+    }
+  }
+}
