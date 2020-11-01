6cdc8a55999d284645db854f35f2603803fa26f9
ivancas84 <ivancas84@gmail.com>
2020-11-01 19:27:45 -0300

    Adaptar interface a la nueva estructura de inicializacion

--- src/app/component/afiliacion-show/afiliacion-search/afiliacion-search.component.ts
+++ src/app/component/afiliacion-show/afiliacion-search/afiliacion-search.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { SearchComponent } from '@component/search/search.component';
-import { Display } from '@class/display';
 
 @Component({
   selector: 'app-afiliacion-search',
