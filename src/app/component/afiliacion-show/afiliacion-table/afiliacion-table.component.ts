c783c06dd92503d4e4e3525e38cfbf4379d79b42
ivancas84 <ivancas84@gmail.com>
2020-11-26 08:38:21 -0300

    incorporacion de funcionalidad en interface afiliacion-table

--- src/app/component/afiliacion-show/afiliacion-table/afiliacion-table.component.ts
+++ src/app/component/afiliacion-show/afiliacion-table/afiliacion-table.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, SimpleChanges } from '@angular/core';
 import { TableComponent } from '@component/table/table.component';
-import { Sort } from '@angular/material/sort';
-import { emptyUrl } from '@function/empty-url.function';
-import { compare } from '@function/compare';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { arrayColumn } from '@function/array-column';
+import { Router } from '@angular/router';
+import { DataDefinitionService } from '@service/data-definition/data-definition.service';
 
 @Component({
   selector: 'app-afiliacion-table',
@@ -13,21 +15,59 @@ import { compare } from '@function/compare';
   `],
 })
 export class AfiliacionTableComponent extends TableComponent { 
-  displayedColumns: string[] = ['persona', 'motivo', 'estado', 'creado', 'enviado', 'evaluado', 'modificado', 'observaciones'];
+  displayedColumns: string[] = ['per-apellidos', 'per-legajo', 'per_dj-codigo', 'motivo', 'estado', 'creado', 'enviado', 'evaluado', 'modificado'];
 
-  serverSort(sort: Sort): boolean{
-    /**
-     * @override
-     */
-    if(sort.active == "persona"){
-      const order = {"per-apellidos":sort.direction};
-      this.display.setOrder(order);
-      this.display.setPage(1);
-
-      this.router.navigateByUrl('/' + emptyUrl(this.router.url) + '?' + this.display.encodeURI());  
-      return true;
-    }
-    return super.serverSort(sort);
+  load$: Observable<any>;
+  load: boolean = false;
+  data$: BehaviorSubject<any> = new BehaviorSubject(null);
+  dataSource: any;
+  
+  constructor(
+    protected router: Router,
+    protected dd: DataDefinitionService,
+  ) {
+    super(router);
   }
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    if( changes['data'] && changes['data'].previousValue != changes['data'].currentValue ) {    
+        this.data$.next(changes['data'].currentValue);
+    }
+  }
+
+  ngOnInit(): void {
+    this.load$ = this.data$.pipe(  
+      switchMap(      
+        data => {
+          this.load = false;
+          return this.initData(data);
+        }
+      ),
+      map(
+        data => {          
+          this.dataSource = data;
+          return this.load = true;
+        }
+      )
+    )
+  }
+  
+  initData(data): Observable<any>{
+    var idsPersonas = arrayColumn(data, "persona");
+    return this.dd.getAll("persona", idsPersonas).pipe(
+      map(
+        personas => {
+          for(var i = 0; i < data.length; i++){
+            data[i]["legajo"] = personas[i]["legajo"];
+            data[i]["nombre"] = personas[i]["apellidos"] + ", " + personas[i]["nombres"];
+            data[i]["departamento_judicial"] = personas[i]["departamento_judicial"];
+            data[i]["departamento_judicial_informado"] = personas[i]["departamento_judicial_informado"];
+          }
+          return data;
+        }
+      )
+    );
+  }
+  
   
 }
