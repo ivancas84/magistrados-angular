44fc6e720b0d671823a26cb66beb80ce9ff7c7d5
unknown <ivancas84@gmail.com>
2021-02-09 12:08:42 -0300

    Agregar tipo documento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,6 +17,7 @@ import { ImporteTramiteExcepcionalShowComponent } from '@component/importe-trami
 import { ArchivoSueldosCreateComponent } from '@component/archivo-sueldos-create/archivo-sueldos-create/archivo-sueldos-create.component';
 import { TotalSummaryComponent } from '@component/total-summary/total-summary/total-summary.component';
 import { SucursalAdminArrayComponent } from '@component/sucursal-admin-array/sucursal-admin-array.component';
+import { TipoDocumentoAdminArrayComponent } from '@component/tipo-documento-admin-array/tipo-documento-admin-array.component';
 
 const routes: Routes = [
   { path: 'backup', component: BackupComponent, pathMatch: 'full' },
@@ -36,6 +37,7 @@ const routes: Routes = [
   { path: 'importe-delete', component: ImporteDeleteComponent, pathMatch: 'full' },
   { path: 'total-summary', component: TotalSummaryComponent, pathMatch: 'full' },
   { path: 'sucursal-admin', component: SucursalAdminArrayComponent, pathMatch: 'full' },
+  { path: 'tipo-documento-admin-array', component: TipoDocumentoAdminArrayComponent, pathMatch: 'full' },
   
   { path: 'login', component: LoginComponent, pathMatch: 'full' },
   { path: '', component: PersonaAdminComponent, pathMatch: 'full' },
