128fd4bb3163352b60dc7d42314df1e52deb5c5a
ivancas84 <ivancas84@gmail.com>
2022-05-18 23:39:17 -0300

    Adaptacion de componente ArchivoSueldosUpload

--- src/app/component/archivo-sueldos-upload/archivo-sueldos-upload.component.ts
+++ src/app/component/archivo-sueldos-upload/archivo-sueldos-upload.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { UploadComponent } from '@component/upload/upload.component';
-import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { FormGroup, Validators, FormBuilder, FormControl } from '@angular/forms';
 import { DialogAlertComponent } from '@component/dialog-alert/dialog-alert.component';
 import { DataDefinitionService } from '@service/data-definition/data-definition.service';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Location } from '@angular/common';
 import { SessionStorageService } from '@service/storage/session-storage.service';
-import { FormControlConfig } from '@class/reactive-form-config';
 import { InputYmConfig } from '@component/input-ym/input-ym.component';
 import { InputSelectConfig } from '@component/input-select/input-select.component';
 
@@ -16,21 +15,21 @@ import { InputSelectConfig } from '@component/input-select/input-select.componen
   templateUrl: './archivo-sueldos-upload.component.html',
 })
 export class ArchivoSueldosUploadComponent extends UploadComponent {
-  readonly entityName: string = "archivo_sueldos"; // archivo sueldos = archivo de afiliaciones + archivo de tramites exepcionales
+  override entityName: string = "archivo_sueldos"; // archivo sueldos = archivo de afiliaciones + archivo de tramites exepcionales
   isSaved = false;
 
   constructor(
-    protected fb: FormBuilder, 
-    protected dd: DataDefinitionService, 
-    protected dialog: MatDialog,
-    protected snackBar: MatSnackBar,
-    protected location: Location,
+    protected override fb: FormBuilder, 
+    protected override dd: DataDefinitionService, 
+    protected override dialog: MatDialog,
+    protected override snackBar: MatSnackBar,
+    protected override location: Location,
     protected storage: SessionStorageService 
   ) {
     super(fb, dd, dialog, snackBar, location);
   }
 
-  uploadForm: FormGroup = this.fb.group(
+  override control: FormGroup = this.fb.group(
     {
       file: [null, {
         validators: [Validators.required],
@@ -46,20 +45,16 @@ export class ArchivoSueldosUploadComponent extends UploadComponent {
     }
   );
 
-  configPeriodo: FormControlConfig = new InputYmConfig({
+  configPeriodo: InputYmConfig = new InputYmConfig({
     label:"Periodo"
   });
-  configOrgano: FormControlConfig = new InputSelectConfig({
+  configOrgano: InputSelectConfig = new InputSelectConfig({
     label:"Organo",
     entityName:"organo"
   })
 
-  formData(): FormData{
-    /**
-     * @override
-     */
-
-    const file = this.file.value._files[0];
+  override formData(): FormData{
+    const file = this.file!.value._files[0];
     const formData = new FormData();
     formData.append("file", file);
     formData.append("organo", this.organo.value);
@@ -67,8 +62,8 @@ export class ArchivoSueldosUploadComponent extends UploadComponent {
     return formData;
   }
 
-  get organo() { return this.uploadForm.controls['organo']}
-  get periodo() { return this.uploadForm.controls['periodo']}
+  get organo() { return this.control.controls['organo'] as FormControl}
+  get periodo() { return this.control.controls['periodo'] as FormControl}
 
 
   progress = 0;
@@ -76,8 +71,8 @@ export class ArchivoSueldosUploadComponent extends UploadComponent {
 
   save(progress = 0){
     this.saveStatus = "saving";
-    var s = this.dd._post("persist", "archivo_sueldos", {log:this.response["log"], progress:progress}).subscribe(
-      (res) => {
+    var s = this.dd._post("persist", "archivo_sueldos", {log:this.response["log"], progress:progress}).subscribe({
+      next: (res) => {
         this.progress = (res["progress"]+1) * 100 / res["total"];
         if(res["progress"]+1 < (res["total"])) this.save(res["progress"]+1);
         else {
@@ -86,27 +81,28 @@ export class ArchivoSueldosUploadComponent extends UploadComponent {
           this.snackBar.open("Procesamiento realizado", "X");
         }
       },
-      (err)=> {
+      error: (err)=> {
         this.saveStatus = "unsaved";
         this.dialog.open(DialogAlertComponent, {
           data: {title: "Error", message: err.error}
         });
       }
-    );
+    })
+    this.subscriptions.add(s);
   }
   
 
   delete() {
-    var s = this.dd._post("delete", "archivo_sueldos", {evaluado:this.response["evaluado"]}).subscribe(
-      (res) => {
+    var s = this.dd._post("delete", "archivo_sueldos", {evaluado:this.response["evaluado"]}).subscribe({
+      next: (res) => {
         this.snackBar.open("Se han eliminado los registros recientes", "X");
       },
-      (err) => {
+      error: (err) => {
         this.dialog.open(DialogAlertComponent, {
           data: {title: "Error", message: err.error}
         });
       }
-    );
+    });
     this.subscriptions.add(s);
   }
 
