d09264ab0f9ce28e86f72b4da26c02e00bc4c385
ivancas84 <ivancas84@gmail.com>
2022-05-24 00:38:42 -0300

    Incorporar componente delete

--- src/app/component/importe-delete/importe-delete.component.ts
+++ src/app/component/importe-delete/importe-delete.component.ts
@@ -1,62 +1,71 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, Validators } from '@angular/forms';
-import { FormStructureConfig } from '@class/reactive-form-config';
+import { FormGroupConfig } from '@class/reactive-form-config';
 import { RequiredValidatorMsg } from '@class/validator-msg';
-import { AdminComponent } from '@component/admin/admin.component';
 import { Observable } from 'rxjs';
-import * as moment from 'moment';
-import { FieldsetDynamicConfig } from '@component/fieldset/fieldset-dynamic.component';
 import { InputYmConfig } from '@component/input-ym/input-ym.component';
 import { InputSelectConfig } from '@component/input-select/input-select.component';
-import { InputSelectLabelConfig } from '@component/input-select-label/input-select-label.component';
-import { FieldWidthOptions } from '@class/field-width-options';
+import { DetailComponent } from '@component/structure/detail.component';
+import { AbstractControlViewOption } from '@component/abstract-control-view/abstract-control-view.component';
+import { EventButtonConfig } from '@component/event-button/event-button.component';
+import { EventIconConfig } from '@component/event-icon/event-icon.component';
 
 @Component({
   selector: 'app-archivo-sueldos-create',
-  templateUrl: '../../core/component/admin/admin.component.html',
+  templateUrl: '../../core/component/structure/detail.component.html',
 })
-export class ImporteDeleteComponent extends AdminComponent implements OnInit {
+export class ImporteDeleteComponent extends DetailComponent implements OnInit {
   
-  entityName: string = "importe"
+  override entityName: string = "importe"
 
-  inputSearchGo: boolean = false
+  override inputSearchGo: boolean = false
 
-  form: FormGroup = this.fb.group({
-    "importe": this.fb.group({
-      "periodo":this.fb.control(moment(), Validators.required),
-      "organo":this.fb.control(null, Validators.required),
-    })
+  override title: string = "Eliminar importes";
+
+  
+  override config: FormGroupConfig = new FormGroupConfig({
+      "periodo": new InputYmConfig({
+        validatorMsgs: [ new RequiredValidatorMsg, ],
+      }),
+      "organo": new InputSelectConfig({
+        validatorMsgs: [ new RequiredValidatorMsg, ],
+      }),
   })
-
-  config: FormStructureConfig = new FormStructureConfig({
-    controls: {
-      "importe": new FieldsetDynamicConfig({
-        entityName:this.entityName,
-        title:"Eliminar ",
-        controls:{
-          "periodo": new InputYmConfig({
-            label: "Período",
-            validatorMsgs: [ new RequiredValidatorMsg, ],
-            width: new FieldWidthOptions()
-          }),
-          "organo": new InputSelectConfig({
-            entityName: "organo",
-            label: "Órgano",
-            validatorMsgs: [ new RequiredValidatorMsg, ],
-            width: new FieldWidthOptions()
-          }),
-        }
-      })
-    }
-  });
-
   
-  persist(): Observable<any> {
+  override optFooter: AbstractControlViewOption[] = [ //opciones de componente
+    {
+      config:new EventButtonConfig({
+        text: "Aceptar", //texto del boton
+        action: "submit", //accion del evento a realizar
+        color: "primary",
+        fieldEvent: this.optField
+      }),
+    },
+    {
+      config:new EventIconConfig({
+        icon: "arrow_back", //texto del boton
+        action: "back", //accion del evento a realizar
+        color: "accent",
+        fieldEvent: this.optField
+      }),
+    },
+    {
+      config:new EventIconConfig({
+        icon: "cached", //texto del boton
+        action: "reset", //accion del evento a realizar
+        color: "primary",
+        fieldEvent: this.optField
+      }),
+    },
+
+  ];
+  
+  override persist(): Observable<any> {
     return this.dd._post("delete", this.entityName, this.serverData());
   }
 
-  reload(){
+  override reload(){
     console.log("Imprimir mensaje de importes eliminados");
   }
+
 }
 
