8e830ebd231ea0523c0b11b1c98da39463378a22
ivancas84 <ivancas84@gmail.com>
2022-05-26 10:32:51 -0300

    Agregar limpieza del storage al cargar inicialmente la interfaz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SessionStorageService } from '@service/storage/session-storage.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'magistrados-angular';
+
+  constructor(
+    protected storage: SessionStorageService, 
+  ) { }
+
+  ngOnInit(): void {
+    this.storage.clear();
+  }
+  
 }
